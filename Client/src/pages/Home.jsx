30e67cdd91997e8b624ed3fd3a0188b58200c239
Anurag-Singh12 <anuragsingh13creator@gmail.com>
2025-11-02 23:22:12 +0530

    Fs,Movie card w TimeFormat(lib),TS,Footer

--- Client/src/pages/Home.jsx
+++ Client/src/pages/Home.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import HeroSection from '../Components/HeroSection'
 import FeaturedSection from '../Components/FeaturedSection'
+import TrailersSection from '../Components/TrailersSection'
 
 const Home = () => {
   return (
     <div>
       <HeroSection/>
       <FeaturedSection/>
+      <TrailersSection/>
+      
     </div>
   )
 }
