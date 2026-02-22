ac35917fbff8781cb4e2340b56c256ba088b8fca
Anurag-Singh12 <anuragsingh13creator@gmail.com>
2026-02-22 21:06:23 +0530

    querySRV ECONNREFUSED Error resolved

--- Server/src/server.js
+++ Server/src/server.js
@@ -1,17 +1,19 @@
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
+import connectDB from './Db/connectionDB.js';
 
 const app = express();
-const port = 3000;
 
 // Middleware
-app.use(express.json())
+app.use(express.json())                                         //connecting backend with any frontend url
 app.use(cors())
+
+await connectDB();
 
 // API Routes
 app.get('/', (req, res) => res.send('Server is Live!'))
 
-app.listen(port, () => 
-  console.log(`Server listening at http://localhost:${port}`)
+app.listen(process.env.PORT, () => 
+  console.log(`Server listening at http://localhost:${process.env.PORT}`)
 );
