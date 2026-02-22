1d9bbf767d5549e1353b5a2cb9248efd87ecd22e
Anurag-Singh12 <anuragsingh13creator@gmail.com>
2026-02-22 22:50:57 +0530

    setup clerk,inngest and user model with inngest functions

--- Server/src/server.js
+++ Server/src/server.js
@@ -2,17 +2,22 @@ import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 import connectDB from './Db/connectionDB.js';
+import { clerkMiddleware } from '@clerk/express'
+import { serve } from "inngest/express";
+import { inngest, functions } from "./inngest/indexinngest.js"
 
 const app = express();
 
-// Middleware
-app.use(express.json())                                         //connecting backend with any frontend url
-app.use(cors())
-
 await connectDB();
+
+// Middleware
+app.use(express.json())
+app.use(cors())                                               //connecting backend with any frontend url
+app.use(clerkMiddleware())
 
 // API Routes
 app.get('/', (req, res) => res.send('Server is Live!'))
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 app.listen(process.env.PORT, () => 
   console.log(`Server listening at http://localhost:${process.env.PORT}`)
