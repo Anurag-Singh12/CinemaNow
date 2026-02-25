0696c9f2114a0915e01380d997312d8fea6d124b
Anurag-Singh12 <anuragsingh13creator@gmail.com>
2026-02-25 20:35:05 +0530

    auth & controller to add new show into DB

--- Server/src/Routes/show.route.js
+++ Server/src/Routes/show.route.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { getNowPlayingMovies } from "../Controllers/show.controller.js";
+import { addShow, getNowPlayingMovies } from "../Controllers/show.controller.js";
+import { protectAdmin } from "../Middlewares/auth.middleware.js";
 
 const showRouter = express.Router();
 
-showRouter.get("/now-playing", getNowPlayingMovies);
+showRouter.get("/now-playing", protectAdmin, getNowPlayingMovies);
+showRouter.post("/add", protectAdmin, addShow);
 
 export default showRouter;
